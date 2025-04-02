39443c648a072b8da3b992a69bcbb8a1e2d3ceb0
DavHau <hsngrmpf+github@gmail.com>
2025-04-02 12:06:12 +0700

    disable upstream services installation hack
    ... this breaks sdcard on second boot

--- nix/goggle-app.nix
+++ nix/goggle-app.nix
@@ -37,9 +37,16 @@ stdenv.mkDerivation {
   buildPhase = ''
     mkdir build
     cmake . -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=${toolchain}/share/buildroot/toolchainfile.cmake -Bbuild
+
     cd build
     make all -j$NIX_BUILD_CORES
     cd ..
+
+    # Disable the services installation hack that upstream uses
+    # (it breaks the sd card on first boot)
+    echo -e "#!/bin/sh\ntrue" > mkapp/app/services/install.sh
+    echo -e "#!/bin/sh\ntrue" > mkapp/app/services/startup.sh
+
     mkfs.ext4 -d ./mkapp/app app.ext2 "100M"
     mv ./out $out
     mv app.ext2 $out/
