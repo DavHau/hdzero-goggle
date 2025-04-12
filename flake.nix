6440aae86ea2fa1b3d6a8e1e408747fbc3932dfe
DavHau <hsngrmpf+github@gmail.com>
2025-04-12 14:23:38 +0700

    flake: cleanup and re-order

--- flake.nix
+++ flake.nix
@@ -55,6 +55,9 @@
     # Packages that can be built via `nix build .#<name>`
     packages.${system} = {
 
+      # make the goggle app the default package
+      default = self.packages.${system}.goggle-app;
+
       # the goggle app including the jffs2 image with service binaries
       goggle-app = pkgs.callPackage ./nix/goggle-app.nix {
         inherit hdzero-goggle-src;
@@ -65,27 +68,23 @@
         inherit hdzero-goggle-src nix-filter;
       };
 
-      goggle-app-nix-static = pkgsArm.pkgsStatic.callPackage ./nix/goggle-app-nix {
-        inherit hdzero-goggle-src nix-filter;
+      emulate = pkgs.callPackage ./nix/emulate {
+        rootfs = self.packages.${system}.rootfs-nixos;
+        pkgsLinux = nixpkgs_22_05.legacyPackages.${system}.pkgsCross.armv7l-hf-multiplatform;
+        inherit (self.packages.${system}) kernel;
+        # inherit hdzero-goggle-linux-src;
       };
 
-      # make the goggle app the default package
-      default = self.packages.${system}.goggle-app;
-
-      # patched toolchain to make compatible with nix build sandbox
-      toolchain = pkgs.callPackage ./nix/toolchain.nix {};
-
-      # the goggle emulator app to run on the dev machine
-      goggle-app-emu = pkgs.callPackage ./nix/goggle-app-emu.nix {
-        inherit hdzero-goggle-src;
+      # the kernel built with nix
+      kernel = nixpkgs_22_05.legacyPackages.${system}.pkgsCross.armv7l-hf-multiplatform.callPackage ./nix/kernel {
+        inherit hdzero-goggle-linux-src;
+        breakpointHook = pkgs.breakpointHook;
       };
 
-      # legacy: this is simply wrapping the full buildroot build from https://github.com/bkleiner/hdzero-goggle-buildroot
-      # better use .#sdcard output instead
-      # contains sdcard.img bootable image, as well as HDZ_OS.bin
-      os-images = pkgs.callPackage ./nix/os-images.nix {
-        inherit hdzero-goggle-src hdzero-goggle-buildroot hdzero-goggle-linux-src;
-        inherit (self.packages.${system}) toolchain;
+      # linux kernel version 4.9.191 from a similar board which is properly open sourced
+      kernel-tinyvision = nixpkgs_22_05.legacyPackages.${system}.pkgsCross.armv7l-hf-multiplatform.callPackage ./nix/kernel-tinyvision {
+        inherit tinyvision-src;
+        breakpointHook = pkgs.breakpointHook;
       };
 
       # only the rootfs etx2 image built by wrapping buildroot
@@ -108,23 +107,6 @@
         goggle-app = self.packages.${system}.goggle-app-nix;
       };
 
-      # tools for creating a bootable image
-      hdzero-goggle-tools = pkgs.callPackage ./nix/hdzero-goggle-tools.nix {};
-
-      # the kernel built with nix
-      kernel =
-      # builtins.trace pkgsArm.stdenv.hostPlatform.linux-kernel.target
-      nixpkgs_22_05.legacyPackages.${system}.pkgsCross.armv7l-hf-multiplatform.callPackage ./nix/kernel {
-        inherit hdzero-goggle-linux-src;
-        breakpointHook = pkgs.breakpointHook;
-      };
-
-      # linux kernel version 4.9.191 from a similar board which is properly open sourced
-      kernel-tinyvision = nixpkgs_22_05.legacyPackages.${system}.pkgsCross.armv7l-hf-multiplatform.callPackage ./nix/kernel-tinyvision {
-        inherit tinyvision-src;
-        breakpointHook = pkgs.breakpointHook;
-      };
-
       # sdcard containing:
       # - nix built kernel
       # - nix built goggle app
@@ -140,31 +122,47 @@
         init = "/linuxrc";
       };
 
+      # sdcard containing:
+      # - nix built kernel
+      # - nix built goggle app
+      # - nix built rootfs
       sdcard-nix = self.packages.${system}.sdcard.override {
         goggle-app = self.packages.${system}.goggle-app-nix;
         rootfs = self.packages.${system}.rootfs-nix;
       };
 
+      # sdcard with full blown nixos system
       sdcard-nixos = self.packages.${system}.sdcard.override {
         goggle-app = self.packages.${system}.goggle-app-nix;
         rootfs = self.packages.${system}.rootfs-nixos;
         init = "/init";
       };
 
-      extfstools = pkgs.callPackage ./nix/extfstools.nix { };
-
+      # filesystem extracted from the original HDZG_OS.bin
       hdzg-os-files = pkgs.callPackage ./nix/hdzg-os-files.nix {};
 
-      emulate = pkgs.callPackage ./nix/emulate {
-        rootfs = self.packages.${system}.rootfs-nixos;
-        pkgsLinux = nixpkgs_22_05.legacyPackages.${system}.pkgsCross.armv7l-hf-multiplatform;
-        inherit (self.packages.${system}) kernel;
-        # inherit hdzero-goggle-linux-src;
+      # some bash scripts which teh goggle app depends on
+      hdzero-scripts = pkgs.callPackage ./nix/hdzero-scripts {};
+
+      # tools for creating a bootable image
+      hdzero-goggle-tools = pkgs.callPackage ./nix/hdzero-goggle-tools.nix {};
+
+      # legacy: patched upstream toolchain to make compatible with nix build sandbox
+      toolchain = pkgs.callPackage ./nix/toolchain.nix {};
+
+      # legacy: this is simply wrapping the full buildroot build from https://github.com/bkleiner/hdzero-goggle-buildroot
+      # better use .#sdcard output instead
+      # contains sdcard.img bootable image, as well as HDZ_OS.bin
+      os-images = pkgs.callPackage ./nix/os-images.nix {
+        inherit hdzero-goggle-src hdzero-goggle-buildroot hdzero-goggle-linux-src;
+        inherit (self.packages.${system}) toolchain;
       };
 
+      # webrtc library (experiment)
       libpeer = pkgs.callPackage ./nix/libpeer.nix {};
 
-      hdzero-scripts = pkgs.callPackage ./nix/hdzero-scripts {};
+      # tools to extract files from ext images. currently not used.
+      extfstools = pkgs.callPackage ./nix/extfstools.nix { };
     };
 
     # a dev environment which can be entered via `nix develop .`
